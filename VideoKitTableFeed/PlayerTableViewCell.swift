b59ff03527601569ca3b2eaaba4e8c96512d3562
Nikita Gaer <gaer.nikita@gmail.com>
2021-01-29 20:47:29 +0400

    Filtered playlist with pagination, auto-playback of video in the middle of the screen, optimizations.

--- VideoKitTableFeed/PlayerTableViewCell.swift
+++ VideoKitTableFeed/PlayerTableViewCell.swift
@@ -12,14 +12,41 @@ class PlayerTableViewCell: UITableViewCell {
     var player: VKPlayerViewController?
     var shouldPlayOnPlayerSet: Bool = false
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.contentView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(togglePlayback)))
+    var playerView = UIView()
+    var playIcon: UIImageView = {
+        let view = UIImageView(image: UIImage(systemName: "play.fill"))
+        view.contentMode = .scaleToFill
+        view.tintColor = .white
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        addSubview(playerView)
+        playerView.clipsToBounds = true
+        playerView.translatesAutoresizingMaskIntoConstraints = false
+        playerView.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        playerView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
+        playerView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        playerView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        
+        addSubview(playIcon)
+        playIcon.translatesAutoresizingMaskIntoConstraints = false
+        playIcon.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        playIcon.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        playIcon.isHidden = true
+        
+        backgroundColor = .clear
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func prepareForReuse() {
-        player?.reset()
-        self.contentView.subviews.forEach({ $0.removeFromSuperview() })
+        self.shouldPlayOnPlayerSet = false
+        self.playerView.subviews.forEach({ $0.removeFromSuperview() })
     }
     
     func setPlayer(player: VKPlayerViewController) {
@@ -27,12 +54,13 @@ class PlayerTableViewCell: UITableViewCell {
 
         DispatchQueue.main.async { [weak self] in
             guard let `self` = self else { return }
-
+            
             player.view.removeFromSuperview()
             player.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            player.view.frame = self.contentView.frame
-            self.contentView.addSubview(player.view)
+            player.view.frame = self.playerView.frame
+            self.playerView.addSubview(player.view)
             self.contentView.layoutIfNeeded()
+            self.playIcon.isHidden = true
             
             if self.shouldPlayOnPlayerSet {
                 self.play()
@@ -40,10 +68,10 @@ class PlayerTableViewCell: UITableViewCell {
         }
     }
     
-    
     func play() {
         shouldPlayOnPlayerSet = true
         player?.play()
+        self.playIcon.isHidden = true
     }
     
     func pause() {
@@ -52,6 +80,9 @@ class PlayerTableViewCell: UITableViewCell {
     }
     
     @objc func togglePlayback() {
-        player?.playState == .playing ? player?.pause() : player?.play()
+        let isPlaying = player?.playState == .playing
+
+        playIcon.isHidden = !isPlaying
+        isPlaying ? pause() : play()
     }
 }
--- VideoKitTableFeed/ViewController.swift
+++ VideoKitTableFeed/ViewController.swift
@@ -12,11 +12,11 @@ import VideoKitRecorder
 class ViewController: UITableViewController {
     private var recorderVC = VKRecorderViewController()
     
-    var playlist = VKPlaylist(videos: [])
+    var playlist = VKPlaylist()
     var playersManager = VKPlayersManager(prerenderDistance: 3, preloadDistance: 10)
     
-    let cellMargin: CGFloat = 50
-    let videoCellFrameHeight: CGFloat = 100
+    var isFetching: Bool = true
+    let cellMargin: CGFloat = 20
     
     var recordButton: UIButton = {
         let button = UIButton(frame: .zero)
@@ -31,12 +31,17 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        self.recorderVC.delegate = self
-        self.recorderVC.dataSource = self
+        self.tableView.estimatedRowHeight = tableView.frame.width * 9 / 16
+        self.tableView.separatorStyle = .none
         
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+
+        // Set recorder delegates
+        self.recorderVC.delegate = self
+        self.recorderVC.dataSource = self
         
+        // Set players manager delegate
         self.playersManager.delegate = self
         self.tableView.register(PlayerTableViewCell.self, forCellReuseIdentifier: "PlayerTableViewCell")
         
@@ -44,41 +49,40 @@ class ViewController: UITableViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(self.sessionStateChanged(_:)), name: .VKAccountStateChanged, object: nil)
         sessionStateChanged()
         
+        // Subscribe on new videos
+        NotificationCenter.default.addObserver(self, selector: #selector(handleVideosCreated(_:)), name: .VKVideoCreated, object: nil)
+        
         tableView.addSubview(recordButton)
         recordButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
         recordButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        NSLayoutConstraint.activate(
-            [
-                NSLayoutConstraint(item: recordButton, attribute: .centerX, relatedBy: .equal, toItem: tableView, attribute: .centerX, multiplier: 1.0, constant: 0.0),
-                NSLayoutConstraint(item: recordButton, attribute: .centerY, relatedBy: .equal, toItem: tableView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
-            ]
-        )
-        
+        recordButton.centerXAnchor.constraint(equalTo: tableView.centerXAnchor).isActive = true
+        recordButton.centerYAnchor.constraint(equalTo: tableView.centerYAnchor).isActive = true
         recordButton.addAction(.init(handler: { (action) in
             self.present(self.recorderVC, animated: true, completion: nil)
         }), for: .touchUpInside)
         
         refresh()
-        NotificationCenter.default.addObserver(self, selector: #selector(handleVideosCreated(_:)), name: .VKVideoCreated, object: nil)
     }
     
     /// This observer also receives uploaded videos from other devices
     @objc private func handleVideosCreated(_ notification: Notification) {
-        if let object = notification.object as? [String : Any], let videoID = object["videoID"] as? String {
-            print("Video with id \(videoID) uploaded.")
-            VKVideoCache.shared.getVideo(videoID) { (video, error) in
-                guard let video = video else { return }
-                
-                // Add received video if it does not exist
-                self.insertVideoToTop(video: video)
-            }
+        if let userInfo = notification.userInfo as? [String : Any],
+           let video = userInfo["video"] as? VKVideo {
+            // Add received video if it does not exist
+            self.insertVideoToTop(video: video)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        scrollViewDidScroll(tableView)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        //self.tableView.visibleCells.forEach({ ($0 as? VideoCell)?.pause() })
+        pausePlayers()
     }
 }
 
@@ -93,47 +97,148 @@ extension ViewController {
     }
     
     @objc func refresh() {
-        _ = VKVideoAPI.shared.videos(byTags: [], metadata: [:], page: 0, perPage: 10, sortOrder: "desc") { [weak self] (response, error) in
-            guard let strongSelf = self else { return }
-            
-            if let error = error {
-                print(error.localizedDescription)
-                return
-            }
-            
-            guard self != nil else { return }
-            guard let response = response else { return }
-            
-            strongSelf.insertNewRows(videos: response.videos)
-        }
+        // Initialize playlist with all videos from the app in descending order
+        // and set it to players manager
+        self.playlist = VKFilteredPlaylist(withFilter: VKVideoFilter(), sortOrder: .desc)
+        self.playlist.delegate = self
+        self.playersManager.setPlaylist(self.playlist)
+        self.tableView.reloadData()
+        self.refreshControl?.endRefreshing()
     }
     
     func insertVideoToTop(video: VKVideo) {
         DispatchQueue.main.async {
             if !self.playlist.hasVideoById(video.videoID) {
                 self.playlist.addVideo(video, at: 0)
-                self.playersManager.setPlaylist(self.playlist)
+                self.playersManager.setPlaylist(self.playlist, index: 0)
+                self.tableView.scrollToRow(
+                    at: IndexPath(row: 0, section: 0),
+                    at: UITableView.ScrollPosition.top,
+                    animated: true
+                )
                 self.tableView.reloadData()
-                self.refreshControl?.endRefreshing()
+                self.pausePlayers()
             }
         }
     }
     
-    func insertNewRows(videos: [VKVideo]) {
-        guard videos.count > 0 else {
-            return
-        }
+    func pausePlayers() {
+        tableView.visibleCells.forEach({ ($0 as? PlayerTableViewCell)?.pause() })
+    }
+}
+
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return playlist.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let video = playlist.videoAt(indexPath.row) else { return 160 }
+        let videoRatio = video.width > 0 ? CGFloat(video.width) / CGFloat(video.height) : 9 / 16
+        let minRatio = tableView.frame.width / (tableView.frame.height - 2 * cellMargin)
+        let ratio = CGFloat.maximum(minRatio, videoRatio)
+
+        return view.frame.width / ratio + cellMargin
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerTableViewCell", for: indexPath) as! PlayerTableViewCell
         
-        DispatchQueue.main.async {
-            for video in videos {
-                if !self.playlist.hasVideoById(video.videoID) {
-                    self.playlist.addVideo(video)
+        // Request prepared video player for new cell
+        playersManager.getPlayerFor(index: indexPath.row, completion: { (player, error) in
+            guard let player = player else { return }
+            
+            DispatchQueue.main.async { [weak self] in
+                // Request cell by indexPath to make sure it's still visible
+                guard let cell = tableView.cellForRow(at: indexPath) as? PlayerTableViewCell else { return }
+                
+                cell.setPlayer(player: player)
+                
+                // Trigger scroll handler for first cell, to start play it.
+                if indexPath.row == 0 {
+                    self?.scrollViewDidScroll(tableView)
                 }
             }
-            self.playersManager.setPlaylist(self.playlist)
-            self.tableView.reloadData()
-            self.refreshControl?.endRefreshing()
+        })
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let video = playlist.videoAt(indexPath.row) else { return }
+        
+        // Release player for cells which is not visible anymore
+        playersManager.releasePlayerFor(id: video.videoID)
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? PlayerTableViewCell else { return }
+
+        cell.togglePlayback()
+        cell.selectionStyle = .none
+    }
+}
+
+extension ViewController {
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if tableView == scrollView {
+            let yOffset = tableView.contentOffset.y
+            let width = tableView.bounds.size.width
+            let height = tableView.bounds.size.height
+            let y = tableView.contentOffset.y == 0 ? 0 : yOffset + height / 4
+
+            // Looking for cells in the middle of the screen
+            if tableView.indexPathsForRows(in: CGRect(x: 0, y: y, width: width, height: height / 2))?.first(where: { path in
+                guard let cell = tableView.cellForRow(at: path) as? PlayerTableViewCell else { return false }
+                guard playlist.videoAt(path.row) != nil else { return false }
+                
+                // Set current video index to players manager. It's important to update players
+                // manager with actual video index, so it can preload and prepare closed videos properly.
+                playersManager.setPlaylistIndex(path.row)
+                
+                // If we have only 5 more videos in the playlist load next bunch of videos.
+                if let filteredPlaylist = playlist as? VKFilteredPlaylist, (
+                    !isFetching &&
+                    path.row + 5 > filteredPlaylist.count &&
+                    filteredPlaylist.count != filteredPlaylist.totalCount
+                ) {
+                    isFetching = true
+                    filteredPlaylist.loadNextVideos()
+                }
+                
+                // Pause all videos and play current video
+                if cell.player?.playState != .playing && viewIfLoaded?.window != nil {
+                    pausePlayers()
+                    cell.play()
+                }
+
+                return true
+            }) == nil {
+                pausePlayers()
+            }
         }
+    }
+}
+
+extension ViewController: VKPlaylistDelegate {
+    func loaded(videos: [VKVideo]) {
+        isFetching = false
+        
+        // Playlist loaded more videos, update table view
+        DispatchQueue.main.async { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
+}
+
+extension ViewController: VKPlayersManagerProtocol {
+    public func vkPlayersManagerNewPlayerCreated(_ manager: VKPlayersManager, _ player: VKPlayerViewController) {
+        // Setup video player
+        player.aspectMode = .resizeAspectFill
+        player.showControls = false
+        player.showSpinner = true
+        player.showErrorMessages = false
+        player.loop = true
     }
 }
 
@@ -167,74 +272,5 @@ extension ViewController: VKRecorderViewControllerDelegate {
         
         recordingViewController.dismiss(animated: true, completion: nil)
         recorderVC.vkRecorder.endSession()
-    }
-}
-
-extension ViewController {
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return  1
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return playlist.count
-    }
-    
-    func getMinRatio() -> CGFloat {
-        return tableView.frame.width / (tableView.frame.height - videoCellFrameHeight - 2 * cellMargin)
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard let video = playlist.videoAt(indexPath.row) else { return 160 }
-        let ratio = CGFloat.maximum(getMinRatio(), CGFloat(video.width) / CGFloat(video.height))
-        
-        return view.frame.width / ratio + cellMargin + videoCellFrameHeight
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerTableViewCell", for: indexPath) as! PlayerTableViewCell
-        let index = indexPath.row
-        
-        playersManager.getPlayerFor(index: index, completion: { (player, error) in
-            guard let player = player else { return }
-            
-            cell.setPlayer(player: player)
-            
-            // Always play first row
-            if indexPath.row == 0 {
-                cell.play()
-            }
-        })
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if tableView.indexPathsForVisibleRows?.contains(indexPath) ?? false {
-            if let vCell = cell as? PlayerTableViewCell {
-                playersManager.setPlaylistIndex(indexPath.row)
-                vCell.play()
-            }
-        }
-    }
-    
-    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        guard let video = playlist.videoAt(indexPath.row) else { return  }
-        
-        if let vCell = cell as? PlayerTableViewCell {
-            vCell.pause()
-        }
-        
-        playersManager.releasePlayerFor(id: video.videoID)
-    }
-}
-
-extension ViewController: VKPlayersManagerProtocol {
-    public func vkPlayersManagerNewPlayerCreated(_ manager: VKPlayersManager, _ player: VKPlayerViewController) {
-        // Setup video player
-        player.aspectMode = .resizeAspectFill
-        player.showControls = false
-        player.showSpinner = true
-        player.showErrorMessages = false
-        player.loop = true
     }
 }
